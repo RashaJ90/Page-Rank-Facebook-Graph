9d92df664e20fd6d457b9b476c23cdfc1b9a020b
RashaJ90 <RashaJ90@users.noreply.github.com>
2023-05-19 22:47:19 +0300

    changes admitted in main

--- communites.py
+++ communites.py
@@ -27,15 +27,4 @@ class Community(object):
                 # External edge, mark as 0
                 self.g.edges[v, w]['community'] = 0
 
-    def get_color(self, i, r_off=1, g_off=1, b_off=1):
-        """
-          Assign a color to a vertex.
-        """
-        r0, g0, b0 = 0, 0, 0
-        n = 16
-        low, high = 0.1, 0.9
-        span = high - low
-        r = low + span * (((i + r_off) * 3) % n) / (n - 1)
-        g = low + span * (((i + g_off) * 5) % n) / (n - 1)
-        b = low + span * (((i + b_off) * 7) % n) / (n - 1)
-        return (r, g, b)
+
